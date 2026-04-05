951f4e67943572d75122a4799a53105640fcbbe6
Mustafa <mustafawork911@gmail.com>
2026-04-06 01:11:19 +0200

    adding DbContext

---
+++ src/DAL/Context/PharmacyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class PharmacyDbContext : DbContext
+    {
+        public PharmacyDbContext(DbContextOptions<PharmacyDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Batch> Batches { get; set; }
+        public DbSet<BatchItem> BatchItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PharmacyDbContext).Assembly);
+            
+        }
+    }
+}
