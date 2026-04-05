cb24161d014eb6e4f2b21e6a41d5ccde570f5e5a
Mustafa <mustafawork911@gmail.com>
2026-04-06 00:40:56 +0200

    Add entity configuration classes

---
+++ src/DAL/Configurations/ProductConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DAL.Configurations
+{
+    internal class ProductConfiguration: IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            
+        }
+    }
+}
