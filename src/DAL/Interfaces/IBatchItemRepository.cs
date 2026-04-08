0182aab217367da2a224f3ca519b2439c7b676bb
abdalhadydev <abdalhady.dev@gmail.com>
2026-04-08 23:00:56 +0200

    added DAL interfaces

---
+++ src/DAL/Interfaces/IBatchItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Models;
+namespace DAL.Interfaces
+{
+    public interface IBatchItemRepository : IGenericRepository<BatchItem>
+    {
+
+    }
+}
