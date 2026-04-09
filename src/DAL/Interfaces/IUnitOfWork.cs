2997105cfebbadb9f71651c196c522b45191f00a
Mustafa <mustafawork911@gmail.com>
2026-04-09 02:47:00 +0200

    added Unit Of Work

---
+++ src/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DAL.Interfaces
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        ISupplierRepository Suppliers { get; }
+        ICustomerRepository Customers { get; }
+        IProductRepository Products { get; }
+        IBatchRepository Batches { get; }
+        IInvoiceRepository Invoices { get; }
+        IBatchItemRepository BatchItems { get; }
+        IInvoiceItemRepository InvoiceItems { get; }
+        Task<int> SaveChangesAsync();
+        Task<IDbContextTransaction> BeginTransactionAsync();
+
+    }
+}
---
+++ src/DAL/Repositories/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Context;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DAL.Repositories
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private readonly PharmacyDbContext _context;
+
+        public ISupplierRepository Suppliers { get; private set; }
+        public ICustomerRepository Customers { get; private set; }
+        public IProductRepository Products { get; private set; }
+        public IBatchRepository Batches { get; private set; }
+        public IInvoiceRepository Invoices { get; private set; }
+        public IBatchItemRepository BatchItems { get; private set; }
+        public IInvoiceItemRepository InvoiceItems { get; private set; }
+
+
+        public UnitOfWork(PharmacyDbContext context,
+            ISupplierRepository supplierRepository,
+            ICustomerRepository customerRepository,
+            IProductRepository productRepository,
+            IBatchRepository batchRepository,
+            IInvoiceRepository invoiceRepository,
+            IBatchItemRepository batchItemRepository,
+            IInvoiceItemRepository invoiceItemRepository)
+        {
+            _context = context;
+            Suppliers = supplierRepository;
+            Customers = customerRepository;
+            Products = productRepository;
+            Batches = batchRepository;
+            Invoices = invoiceRepository;
+            BatchItems = batchItemRepository;
+            InvoiceItems = invoiceItemRepository;
+        }
+
+        public async Task<int> SaveChangesAsync() =>
+            await _context.SaveChangesAsync();
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync() =>
+            await _context.Database.BeginTransactionAsync();
+
+        public void Dispose() => _context.Dispose();
+    }
+}
