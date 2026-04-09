45e570637f64c1b5e4bdc42ebbaae86b9c565029
Mustafa <mustafawork911@gmail.com>
2026-04-09 03:25:40 +0200

    added shared enums to Common project

---
+++ src/Common/Enums/StockAdjustmentReason.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Enums
+{
+    public enum StockAdjustmentReason
+    {
+        ExpiryDisposal,
+        SupplierReturn,
+        CountCorrection,
+        Damaged,
+        Theft,
+        Other
+    }
+}
