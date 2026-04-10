a68543ac12e2ae920fa1fcb885b4cfae00ce1b26
Mustafa <mustafawork911@gmail.com>
2026-04-10 13:47:29 +0200

    added Service, Interfaces, and DTOs folders

---
+++ src/BLL/DTOs/Batch/BatchSummaryDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class BatchSummaryDto
+    {
+        public int Id { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public int SupplierId { get; set; }
+        public string SupplierName { get; set; }
+        
+    }
+}
