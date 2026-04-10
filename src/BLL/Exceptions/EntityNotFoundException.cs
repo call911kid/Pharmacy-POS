0feade20eb08b46ee084b79c435c7a7ed6f11364
Mustafa <mustafawork911@gmail.com>
2026-04-10 14:34:16 +0200

    added custom exceptions folder and implemented EntityNotFoundException

---
+++ src/BLL/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException(string message)
+            : base(message) { }
+    }
+}
