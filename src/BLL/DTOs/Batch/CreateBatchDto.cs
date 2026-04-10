10d36726cd8af60706cd43090551ef46db8512f0
Mustafa <mustafawork911@gmail.com>
2026-04-10 22:32:09 +0200

    added create Batch and BatchItem DTOs

--- src/BLL/DTOs/Batch/CreateBatchDto.cs
+++ src/BLL/DTOs/Batch/CreateBatchDto.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLL.DTOs.BatchItem;
 
 namespace BLL.DTOs.Batch
 {
     public class CreateBatchDto
     {
+        public int SupplierId { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public List<CreateBatchItemDto> BatchItems { get; set; }
+
+        public CreateBatchDto()
+        {
+            BatchItems = new List<CreateBatchItemDto>();
+        }
     }
 }
---
+++ src/BLL/DTOs/BatchItem/CreateBatchItemDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs.BatchItem
+{
+    public class CreateBatchItemDto
+    {
+        public int BatchId { get; set; }
+        public int ProductId { get; set; }
+        public int QuantityReceived { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal MandatorySellingPrice { get; set; }
+
+    }
+}
