08d225ed31a9fec3d678cf8c105cb1203eeeb131
Amr <Amr@DESKTOP-BJ74B5M>
2026-04-12 20:02:42 +0200

    Create ICustomerService

---
+++ src/BLL/Interfaces/ICustomerService.cs
@@ -0,0 +1,25 @@
+﻿using BLL.DTOs;
+using BLL.DTOs.Customer;
+using DAL.Models;
+
+namespace BLL.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<IReadOnlyList<Customer?>> GetCustomersByBatchItemAsync(int batchItemId);
+
+        Task<IReadOnlyList<Invoice?>> GetInvoiceHistoryAsync(int customerId);
+
+        Task<CustomerValidationResultDto> ValidateForSaleAsync(int customerId);
+
+        Task<CustomerDto?> CreateCustomerAsync(CreateCustomerByPhoneDto dto);
+
+        Task<CustomerDto?> GetCustomerByPhoneAsync(string phone);
+
+        Task<CustomerDto?> GetCustomerAsync(int customerId);
+
+        Task<IReadOnlyList<CustomerDto>> FindByNameAsync(string name);
+
+        Task<CustomerDto?> UpdateContactInfoAsync(int customerId, string name, string phone);
+    }
+}
