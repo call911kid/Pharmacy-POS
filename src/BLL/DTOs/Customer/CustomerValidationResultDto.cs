4ce3512d2b11308f69f8c8fd5c0ca286550dbba2
Amr <Amr@DESKTOP-BJ74B5M>
2026-04-12 20:01:15 +0200

    Add Create Customer & CustomerDto & CustomerValidation

---
+++ src/BLL/DTOs/Customer/CustomerValidationResultDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs.Customer
+{
+    public class CustomerValidationResultDto
+    {
+        public int CustomerId { get; set; }
+        public bool IsValid { get; set; }
+        public string? Reason { get; set; }
+    }
+}
